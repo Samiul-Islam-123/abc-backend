1343aa33a82f1d5628503d86e0da0fc0cea097d9
MD Samiul Islam <isamiul099@gmail.com>
2024-08-15 20:28:08 +0530

    implemented github data api:

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const checkAuth = require('./middlewares/Auth');
 const app = express();
 const cors = require('cors');
 const PostRouter = require('./routes/PostRouter');
+const axios = require('axios')
 
 const PORT = process.env.PORT || 5500;
 
@@ -31,6 +32,32 @@ app.use('/event', EventRouter)
 app.use('/project',  ProjectRouter)
 app.use('/util', UploadRouter);
 app.use("/post", PostRouter)
+
+const githubToken = process.env.GITHUB_ACCESS_TOKEN;
+
+
+app.get("/github-data/commits", async(req,res) => {
+    try {
+        const { owner, repo } = req.query;
+    
+        const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/commits`, {
+          headers: {
+            Authorization: `token ${githubToken}`,
+          },
+        });
+    
+        const commits = response.data.map(commit => ({
+          sha: commit.sha,
+          author: commit.commit.author.name,
+          date: commit.commit.author.date,
+          message: commit.commit.message,
+        }));
+        res.json(commits);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error fetching commits' });
+      }
+})
 
 app.get("/dummy-dashboard", (req,res) => {
     const dummyData = {
